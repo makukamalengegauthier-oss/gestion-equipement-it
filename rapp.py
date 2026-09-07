e2c30abb3c639364322bb2eebebd353a62ba0541
makukamalengegauthier-oss <makukamalengegauthier@gmail.com>
2026-09-07 14:07:26 +0000

    Fix robust pdf_bytes definition

--- rapp.py
+++ rapp.py
@@ -1242,9 +1242,8 @@ elif menu == "Rapport Général & Export PDF":
     add_table_to_pdf(pdf, "3. Réaffectations", df_reaff)
     add_table_to_pdf(pdf, "4. Désaffectations", df_desaf)
     
-    pdf_bytes = pdf.output()
-if isinstance(pdf_bytes, str):
-    pdf_bytes = pdf_bytes.encode('latin1')
+ pdf_output = pdf.output()
+pdf_bytes = pdf_output if isinstance(pdf_output, bytes) else pdf_output.encode('latin1')
     
     # --- 6. EXPORTATION ET ENVOI PAR MESSAGERIE DIRECTE ---
     st.markdown("---")
